f695d51a401830e690986dcbd4585023f277796b
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 19:07:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,5 +54,11 @@ end
 #####################################################
 
 def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+  end
 end
+
 
